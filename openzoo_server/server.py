00263df5a0ba0f02a6589cead16b8ec0d259d040
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2024-04-15 10:50:46 +0100

    Gets Auth scaffolding for single API key working

--- openzoo_server/server.py
+++ openzoo_server/server.py
@@ -18,20 +18,31 @@ from validation.chat import ChatCompletionRequest
 
 # Instantiate the FastAPI app
 app = FastAPI(title="OpenZoo")
-security = HTTPBearer()
+security = HTTPBearer(auto_error=False)
 
 
 # instantiate the router
 router = Router()
 
 
+# Authorization
+def check_bearer_credentials(token: str = Depends(security)):
+    if token.credentials is None:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
+    if token.credentials != "d56f4hd45hd23h4d86h4e5rgstwetgljew":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    return {"credentials": token}
+
+
 @app.post("/chat/completions")
-async def chat_completions(request: ChatCompletionRequest, header= Depends(security)):
+async def chat_completions(request: ChatCompletionRequest, header= Depends(check_bearer_credentials)):
     """
     Generate completions for a chat prompt.
     """
 
-    print(header.credentials)
+    print(header)
 
     if request.stream:
         return StreamingResponse(
